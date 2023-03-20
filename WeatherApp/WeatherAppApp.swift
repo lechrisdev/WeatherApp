c47d9e173ee693031f730483a51db9ec5d75d971
LeChris <bk2mua@gmail.com>
2023-03-20 11:05:34 +0200

    initial commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Le Chris on 20.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
