f09eacfbecb3e481eb29e3369fafaf91b74f631c
LeChris <bk2mua@gmail.com>
2023-04-24 20:23:14 +0300

    In MainViewModel create a func for loading all saved cities from an array of coordinates

--- WeatherApp/MainView/MainViewModel.swift
+++ WeatherApp/MainView/MainViewModel.swift
@@ -6,23 +6,45 @@
 //
 
 import Foundation
+import CoreLocation
 
 class MainViewModel: ObservableObject {
     
     @Published var models: [WeatherModel] = []
     
+    var arrayOfCoordinates: [CLLocationCoordinate2D] = [
+        
+        CLLocationCoordinate2D(latitude: 46.48253, longitude: 30.72331), // Одесса
+        CLLocationCoordinate2D(latitude: 49.9935, longitude: 36.23038), // Харьков
+        CLLocationCoordinate2D(latitude: 49.83968, longitude: 24.02972) // Львов
+        
+    ]
+    
     let repo = Repository()
     
     init() {
-        Task {
-            if let weather = await repo.getWeather(lon: 30.68363780440557, lat: 46.48590215) {
+        loadArrayOfCities()
+    }
+    
+    func loadWeather(coordinates2D: CLLocationCoordinate2D) async {
+        
+            if let weather = await repo.getWeather(lon: coordinates2D.longitude,
+                                                   lat: coordinates2D.latitude) {
                 DispatchQueue.main.async {
                     self.models.append(weather)
                     print(">>>>", self.models.first)
                 
                 }
-                
+            }
+    }
+    
+    func loadArrayOfCities() {
+        Task {
+            for coordinates in arrayOfCoordinates {
+                await loadWeather(coordinates2D: coordinates)
             }
         }
     }
+//    In MainViewModel create a func for loading all saved cities from an array of coordinates
+    
 }
